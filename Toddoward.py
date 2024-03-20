6cd688c512a784681d93c24a0012fe3ee15dcfe7
EthanMcCagherty <159316318+EthanMcCagherty@users.noreply.github.com>
2024-03-20 11:24:18 -0700

    Update Toddoward.py
    - added second servo to the base code

--- Toddoward.py
+++ Toddoward.py
@@ -100,7 +100,11 @@ factory = PiGPIOFactory()
 servo = Servo(12, min_pulse_width=0.5/1000,
               max_pulse_width=2.5/1000, pin_factory=factory)
 
+servo2 = Servo(13, min_pulse_width=0.5/1000,
+              max_pulse_width=2.5/1000, pin_factory=factory)
+
 while True:
     for i in range(0, 360):
         servo.value = math.sin(math.radians(i))
+        servo2.value = math.sin(math.radians(i))
         sleep(0.01)
